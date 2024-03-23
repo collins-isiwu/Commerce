ba0c93042f33f4a92f0151f56ac87aa80faeff86
Collins Isiwu <collinscisiwu@outlook.com>
2024-03-23 23:55:22 +0100

    'updates'

--- commerce/commerce/settings.py
+++ commerce/commerce/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'auctions/static']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
