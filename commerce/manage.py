60ae27f5c8c66f05e677b674482346dfb656580c
Collins Isiwu <collinscisiwu@outlook.com>
2024-03-23 18:35:01 +0100

    update

--- commerce/manage.py
+++ commerce/manage.py
@@ -3,9 +3,11 @@
 import os
 import sys
 
+# Manage.py for Production in PythonAnywhere
+
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'commerce.settings_dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'commerce.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- commerce/manage_dev.py
+++ commerce/manage_dev.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'commerce.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'commerce.settings_dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
