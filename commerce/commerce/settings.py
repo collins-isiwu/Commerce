1afa577c6f1e9553d768c602534c415db5f4455a
Collins Isiwu <collinscisiwu@outlook.com>
2024-03-24 00:16:04 +0100

    'updates'

--- commerce/commerce/settings.py
+++ commerce/commerce/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
